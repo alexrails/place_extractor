e2c3d188e75e8f9c02f671980f8f677fa8b1d906
Alex Korzun <alex.korzun@productmadness.com>
2025-01-12 22:18:55 +0400

    feat: handle http errors

--- app/place_api.rb
+++ app/place_api.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require 'httparty'
+require 'resolv-replace'
+require 'retryable'
 require_relative '../config'
 require_relative 'place_extractor'
 require_relative 'interface'
+require_relative 'logger'
 
 class PlaceAPI
   BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+  MAX_REQUESTS = 10
+  RETRYABLE_ERRORS = [Net::OpenTimeout, HTTParty::Error, Resolv::ResolvError].freeze
+
   attr_reader :request_counter
 
   def initialize
@@ -14,34 +20,20 @@ class PlaceAPI
   end
 
   def fetch_organizations(coordinates, radius, type)
-    next_page_token = nil
     results = []
+    next_page_token = nil
 
     loop do
       url = build_url(next_page_token, coordinates, radius, type)
-      response = HTTParty.get(url)
-      @request_counter += 1
-
-      if response.code != 200
-        puts "Error: Received #{response.code} from Google Places API"
-        break
-      end
+      response = make_request(url)
+      break unless response
 
       data = response.parsed_response
-      if data['status'] == 'ZERO_RESULTS'
-        Interface.no_results
-        break
-      end
+      break unless handle_response(data, results)
 
-      if data['status'] != 'OK'
-        puts "Error: API Response Status - #{data['status']}"
-        break
-      end
-
-      results.concat(data['results'])
       next_page_token = data['next_page_token']
-
       break unless next_page_token
+
       sleep(2) # Required by Google API for subsequent requests
     end
 
@@ -53,18 +45,50 @@ class PlaceAPI
   def build_url(next_page_token, coordinates, radius, type)
     query_params = {
       key: Config::GOOGLE_PLACES_API_KEY,
-      location: coordinates
+      location: coordinates,
+      type: determine_type(radius, type)
     }
-
-    if radius > PlaceExtractor::MINIMAL_RADIUS
-      query_params[:radius] = radius
-      query_params[:type] = type
-    else
-      query_params[:rankby] = :distance
-      query_params[:type] = type.empty? ? PlaceExtractor::DEFAULT_TYPE : type
-    end
-
+    query_params[:radius] = radius if radius > PlaceExtractor::MINIMAL_RADIUS
+    query_params[:rankby] = :distance if radius <= PlaceExtractor::MINIMAL_RADIUS
     query_params[:pagetoken] = next_page_token if next_page_token
+
     "#{BASE_URL}?#{URI.encode_www_form(query_params)}"
+  end
+
+  def determine_type(radius, type)
+    radius > PlaceExtractor::MINIMAL_RADIUS ? type : (type.empty? ? PlaceExtractor::DEFAULT_TYPE : type)
+  end
+
+  def make_request(url)
+    Retryable.retryable(tries: 3, on: RETRYABLE_ERRORS, sleep: 1) do
+      response = HTTParty.get(url, timeout: 10)
+      @request_counter += 1
+
+      return response if response.code == 200
+
+      logger.error "Error: Received #{response.code} from Google Places API"
+      nil
+    end
+  rescue => e
+    logger.error("Error: #{e.message}. Returning current results")
+    nil
+  end
+
+  def handle_response(data, results)
+    case data['status']
+    when 'ZERO_RESULTS'
+      Interface.no_results
+      false
+    when 'OK'
+      results.concat(data['results'])
+      true
+    else
+      logger.error("Error: API Response Status - #{data['status']}")
+      false
+    end
+  end
+
+  def logger
+    @logger ||= AppLogger.instance
   end
 end
